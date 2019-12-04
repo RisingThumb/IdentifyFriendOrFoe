e6dff499ec42cdd010c5731bfab6315e9299892d
KevlarBullet <ian.kellogg001@gmail.com>
2019-12-03 20:53:55 -0900

    Re-added missing groups folder (formerly classes)

---
+++ src/main/java/xyz/risingthumb/iff/groups/GroupManager.java
@@ -0,0 +1,54 @@
+package xyz.risingthumb.iff.groups;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupManager {
+	private List<Group> groups;
+	private int groupIndex = 0;
+	
+	public GroupManager() {
+		this.groups = new ArrayList<Group>();
+	}
+	
+	public void addGroup(Group group) {
+		this.groups.add(group);
+	}
+	public void removeGroup(int indexToRemove) {
+		// We remove based on index
+		this.groups.remove(indexToRemove);
+		modifyGroupIndex(-1);
+	}
+	public void modifyGroupIndex(int changeValue) {
+		this.groupIndex+=changeValue;
+		// If there's no groups. literally do nothing
+		if (groups.size()==0) {
+			this.groupIndex = 0;
+			return;
+		}
+		// Bounds
+		if (groupIndex < 0) {
+			groupIndex = groups.size()-1;
+		}
+		if (groupIndex >= groups.size()) {
+			groupIndex = 0;
+		}
+	}
+
+	public int getSize() {
+		return groups.size();
+	}
+
+	public String getNameOfGroup(int index) {
+		return groups.get(index).getName();
+	}
+	
+	public Group getGroup(int index) {
+		return groups.get(index);
+	}
+
+	public List<Group> getGroups() {
+		return groups;
+	}
+
+}
---
+++ src/main/java/xyz/risingthumb/iff/groups/GroupPerson.java
@@ -0,0 +1,14 @@
+package xyz.risingthumb.iff.groups;
+
+public class GroupPerson {
+	String name;
+	
+	public GroupPerson(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+}
